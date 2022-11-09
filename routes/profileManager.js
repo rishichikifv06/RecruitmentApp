244ab5cd9dfaf96549e858946aeeaf116d8450c7
J <Guruprasad.J@ad.jktech.com>
2022-11-09 12:48:08 +0530

    new profile api

--- routes/profileManager.js
+++ routes/profileManager.js
@@ -1,14 +1,37 @@
 var express = require("express");
 var router = express.Router();
+var sql = require("msnodesqlv8");
+var details = require("../db");
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
 
 router.get("/", (req, res) => {
-  res.status(200).send("Home page of Profile Manager");
+  async function getData()
+  {
+    await sql.open(details.connectionString, async (err, conn)=>{
+    await  conn.query("SELECT * FROM Candidates",(err, data)=>{
+        if(data){
+          console.log(data);
+          const result = { data };
+          res.status(200).json(result);
+        }
+        if(err){
+          console.log(err);
+          res.send(err);
+        }
+      })
+      if(err){
+        console.log(err);
+        res.send(err);
+      }
+    })
+  }
+ 
+  getData();
 });
 
 
-router.post("/", (req, res) => {
 
-});
 
 
 
