c9891f7790c27a12a440208b5d7b18a825e9df4d
J <Guruprasad.J@ad.jktech.com>
2022-11-16 13:18:48 +0530

    udated api for end assessment and cards score

--- routes/assessmentManager.js
+++ routes/assessmentManager.js
@@ -17,7 +17,7 @@ router.post("/endAssessment",jsonParser, (req, res) => {
     await  conn.query(`INSERT INTO AssessmentDetails(assessmentId, queId, ansId, score, Note, assessmentDetailsStatus) 
     SELECT assessmentId, queId, ansId, score, Note, AssessmentStagingstatus FROM AssessmentStaging WHERE canId=${canId} AND assessmentId=${assessmentId}`,(err, data)=>{
         if(data){
-           conn.query(`UPDATE Candidates SET Candidatestatus='closed' WHERE canId = ${canId} AND assessmentId = ${assessmentId}`),
+           conn.query(`UPDATE Candidates SET Candidatestatus='${status}' WHERE canId = ${canId} AND assessmentId = ${assessmentId}`),
           (err, output)=>{
             if(err){
               console.log(err);
