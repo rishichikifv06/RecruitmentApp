d36f7b321ebb733acc0932c9da52911e4184502b
J <Guruprasad.J@ad.jktech.com>
2022-11-21 12:15:27 +0530

    new changes

--- routes/cardsScoreManager.js
+++ routes/cardsScoreManager.js
@@ -8,48 +8,116 @@ const cookieParser = require("cookie-parser");
 var app = express();
 //const { Connection, Request } = require("tedious");
 var jsonParser = bodyParser.json();
-router.post('/scores', jsonParser, (req, res) => {
+// router.post('/scores', jsonParser, (req, res) => {
+//     if (req.body != undefined) {
+//         const canId = req.body.canId;
+//         const Date = req.body.Date;
+//         //const assessmentId=req.body.assessmentId;
+//         //const Assessmentstatus=req.body.Assessmentstatus;
+//         async function getScore() {
+//             await sql.open(details.connectionString, async (err, conn) => {
+//                 await conn.query(`select assessmentId from Assessment where canId=${canId} and date='${Date}'`, async (err, data) => {
+//                     if (data) {
+//                         const assessmentId = data[0].assessmentId;
+//                         await conn.query(`
+//                         select Skill.skillName, Complexity.Name, Sum(AssessmentDetails.score) as "Totalscore",count(Questions.skillId)as 
+//                         "count" from AssessmentDetails left join Questions
+//                         on Questions.queId=AssessmentDetails.queId 
+//                         LEFT JOIN Skill ON Questions.skillId=Skill.skillId LEFT JOIN Complexity ON Questions.cmpId=Complexity.cmpId where assessmentId=${assessmentId} 
+//                         group by Questions.skillId,Skill.skillName,Complexity.Name
+//                         `,(err, val) => {
+//                             if (val) {
+//                                 const array = {
+//                                     val
+//                                 }
+//                                 res.status(200).json(array);
+//                             }
+//                             if (err) {
+//                                 console.log(err);
+//                                 res.send(err);
+//                             }
+//                         })
+//                     }
+//                     if (err) {
+//                         console.log(err);
+//                         res.send(err);
+//                     }
+//                 })
+//                 if (err) {
+//                     console.log(err);
+//                     res.send(err);
+//                 }
+//             })
+//         }
+//         getScore();
+//     }
+// })
+
+router.post('/scores', jsonParser,(req, res)=>{
     if (req.body != undefined) {
-        const canId = req.body.canId;
-        const Date = req.body.Date;
-        //const assessmentId=req.body.assessmentId;
-        //const Assessmentstatus=req.body.Assessmentstatus;
-        async function getScore() {
-            await sql.open(details.connectionString, async (err, conn) => {
-                await conn.query(`select assessmentId from Assessment where canId=${canId} and date='${Date}'`, async (err, data) => {
-                    if (data) {
-                        const assessmentId = data[0].assessmentId;
-                        await conn.query(`
-                        select Questions.skillId,Questions.cmpId ,Skill.skillName, Complexity.Name, Sum(AssessmentDetails.score) as "Totalscore",count(Questions.skillId)as 
-                        "count" from AssessmentDetails left join Questions
-                        on Questions.queId=AssessmentDetails.queId 
-                        LEFT JOIN Skill ON Questions.skillId=Skill.skillId LEFT JOIN Complexity ON Questions.cmpId=Complexity.cmpId where assessmentId=${assessmentId} 
-                        group by Questions.skillId,Questions.cmpId,Skill.skillName,Complexity.Name
-                        `,(err, val) => {
-                            if (val) {
-                                const array = {
-                                    val
-                                }
-                                res.status(200).json(array);
+                const canId = req.body.canId;
+                const Date = req.body.Date;
+                const assessmentId=req.body.assessmentId;
+                const Assessmentstatus=req.body.Assessmentstatus;
+                async function getScore() {
+                    await sql.open(details.connectionString, async (err, conn) => {
+                        await conn.query(`select assessmentId from Assessment where canId=${canId} and date='${Date}'`, async (err, data) => {
+                            if (data) {
+                                const assessmentId = data[0].assessmentId;
+                                await conn.query(`
+                                select Skill.skillName, Sum(AssessmentDetails.score) as "candidateScore",count(Questions.skillId)as 
+                                "count" from AssessmentDetails left join Questions
+                                on Questions.queId=AssessmentDetails.queId 
+                                LEFT JOIN Skill ON Questions.skillId=Skill.skillId LEFT JOIN Complexity ON Questions.cmpId=Complexity.cmpId where assessmentId=${assessmentId} 
+                                group by Skill.skillName
+                                `,(err, val) => {
+                                    if (val) {
+                                        val.forEach(element => {
+                                            element.skillScore = element.count*10;
+                                            element.average = Math.round(element.candidateScore/element.skillScore*100);
+                                        });
+                                        console.log(val);
+                                        let totalCount = 0;
+                                        let totalScore = 0;
+                                        let totalAverage = 1;
+                                        let totalCandidateScore = 0;
+                                        val.forEach(element => {
+                                            totalCount = totalCount+element.count;
+                                            totalScore = totalScore+element.skillScore;
+                                            totalCandidateScore = totalCandidateScore+element.candidateScore;  
+                                        })
+                                        totalAverage = Math.round(totalCandidateScore/totalScore*100);
+
+                                        let data = {
+                                            val,
+                                            totalCount: totalCount,
+                                            totalScore: totalScore,
+                                            totalCandidateScore: totalCandidateScore,
+                                            totalAverage: totalAverage
+                                        }
+                                        
+
+
+                                        res.status(200).json(data);
+                                    }
+                                    if (err) {
+                                        console.log(err);
+                                        res.send(err);
+                                    }
+                                })
                             }
                             if (err) {
                                 console.log(err);
                                 res.send(err);
                             }
                         })
-                    }
-                    if (err) {
-                        console.log(err);
-                        res.send(err);
-                    }
-                })
-                if (err) {
-                    console.log(err);
-                    res.send(err);
+                        if (err) {
+                            console.log(err);
+                            res.send(err);
+                        }
+                    })
                 }
-            })
-        }
-        getScore();
-    }
+                getScore();
+            }
 })
 module.exports = router;
