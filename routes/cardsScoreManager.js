1496d7f1d7f9348d005f35a9a75964755de9688c
J <Guruprasad.J@ad.jktech.com>
2022-11-29 17:35:58 +0530

    updates in cardsScore

--- routes/cardsScoreManager.js
+++ routes/cardsScoreManager.js
@@ -7,65 +7,7 @@ const { json } = require("body-parser");
 var jsonParser = bodyParser.json();
 const {ConnectToDb,ExecuteQuery} = require('../db');
 
-// async function getScore() {
-//     await sql.open(details.connectionString, async (err, conn) => {
-//         await conn.query(`select assessmentId from Assessment where canId=${canId} and date='${Date}'`, async (err, data) => {
-//             if (data) {
-//                 const assessmentId = data[0].assessmentId;
-//                 await conn.query(`
-//                 select Skill.skillName, Sum(AssessmentDetails.score) as "candidateScore",count(Questions.skillId)as 
-//                 "count" from AssessmentDetails left join Questions
-//                 on Questions.queId=AssessmentDetails.queId 
-//                 LEFT JOIN Skill ON Questions.skillId=Skill.skillId LEFT JOIN Complexity ON Questions.cmpId=Complexity.cmpId where assessmentId=${assessmentId} 
-//                 group by Skill.skillName
-//                 `,(err, val) => {
-//                     if (val) {
-//                         val.forEach(element => {
-//                             element.skillScore = element.count*10;
-//                             element.percentage = Math.round(element.candidateScore/element.skillScore*100);
-//                         });
-//                         console.log(val);
-//                         let totalCount = 0;
-//                         let totalScore = 0;
-//                         let totalPercentage = 1;
-//                         let totalCandidateScore = 0;
-//                         val.forEach(element => {
-//                             totalCount = totalCount+element.count;
-//                             totalScore = totalScore+element.skillScore;
-//                             totalCandidateScore = totalCandidateScore+element.candidateScore;  
-//                         })
-//                         totalPercentage = Math.round(totalCandidateScore/totalScore*100);
 
-//                         let data = {
-//                             val,
-//                             totalCount: totalCount,
-//                             totalScore: totalScore,
-//                             totalCandidateScore: totalCandidateScore,
-//                             totalPercentage: totalPercentage
-//                         }
-                        
-
-
-//                         res.status(200).json(data);
-//                     }
-//                     if (err) {
-//                         console.log(err);
-//                         res.send(err);
-//                     }
-//                 })
-//             }
-//             if (err) {
-//                 console.log(err);
-//                 res.send(err);
-//             }
-//         })
-//         if (err) {
-//             console.log(err);
-//             res.send(err);
-//         }
-//     })
-// }
-// getScore();
 
 router.post('/scores', jsonParser,(req, res)=>{
     if (req.body != undefined) {
@@ -74,19 +16,78 @@ router.post('/scores', jsonParser,(req, res)=>{
                 const assessmentId=req.body.assessmentId;
                 const Assessmentstatus=req.body.Assessmentstatus;
 
-                async function getScoresForCards(){
+                // async function getScoresForCards(){
                     
-                    await ConnectToDb().then(async (dbConnection)=>{
-                        if(dbConnection){
+                //     await ConnectToDb().then(async (dbConnection)=>{
+                //         if(dbConnection){
 
-                            await ExecuteQuery(dbConnection, `select assessmentId from Assessment where canId=${canId} and date='${Date}'`)
-                            .then()
-                        }else{
-                            console.log("Not connected to db!!!");
+                //             await ExecuteQuery(dbConnection, `select assessmentId from Assessment where canId=${canId} and date='${Date}'`)
+                //             .then()
+                //         }else{
+                //             console.log("Not connected to db!!!");
+                //         }
+                //     })
+                // }
+                // getScoresForCards();
+                async function getScore() {
+                    await sql.open(details.connectionString, async (err, conn) => {
+                        await conn.query(`select assessmentId from Assessment where canId=${canId} and date='${Date}'`, async (err, data) => {
+                            if (data) {
+                                const assessmentId = data[0].assessmentId;
+                                await conn.query(`
+                                select Skill.skillName, Sum(AssessmentDetails.score) as "candidateScore",count(Questions.skillId)as 
+                                "count" from AssessmentDetails left join Questions
+                                on Questions.queId=AssessmentDetails.queId 
+                                LEFT JOIN Skill ON Questions.skillId=Skill.skillId LEFT JOIN Complexity ON Questions.cmpId=Complexity.cmpId where assessmentId=${assessmentId} 
+                                group by Skill.skillName
+                                `,(err, val) => {
+                                    if (val) {
+                                        val.forEach(element => {
+                                            element.skillScore = element.count*10;
+                                            element.percentage = Math.round(element.candidateScore/element.skillScore*100);
+                                        });
+                                        console.log(val);
+                                        let totalCount = 0;
+                                        let totalScore = 0;
+                                        let totalPercentage = 1;
+                                        let totalCandidateScore = 0;
+                                        val.forEach(element => {
+                                            totalCount = totalCount+element.count;
+                                            totalScore = totalScore+element.skillScore;
+                                            totalCandidateScore = totalCandidateScore+element.candidateScore;  
+                                        })
+                                        totalPercentage = Math.round(totalCandidateScore/totalScore*100);
+                
+                                        let data = {
+                                            val,
+                                            totalCount: totalCount,
+                                            totalScore: totalScore,
+                                            totalCandidateScore: totalCandidateScore,
+                                            totalPercentage: totalPercentage
+                                        }
+                                        
+                
+                
+                                        res.status(200).json(data);
+                                    }
+                                    if (err) {
+                                        console.log(err);
+                                        res.send(err);
+                                    }
+                                })
+                            }
+                            if (err) {
+                                console.log(err);
+                                res.send(err);
+                            }
+                        })
+                        if (err) {
+                            console.log(err);
+                            res.send(err);
                         }
                     })
                 }
-                getScoresForCards();
+                getScore();
 
             }
 })
