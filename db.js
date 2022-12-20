985634ad5620f45835c13bd268039324eaea3f52
Guruprasad.J <Guruprasad.J@jktech.com>
2022-12-20 09:29:20 +0530

    db.js changes

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_NAME,
-    port: process.env.PORT
+    port: process.env.DB_PORT
   });
 
   
